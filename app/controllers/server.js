c4157a34e5e02853a12708dfbed8c5fed0aa537a
Gabriel Hase <gabriel.hase@gmail.com>
2012-07-17 18:49:24 +0200

    added games parsing

--- app/controllers/server.js
+++ app/controllers/server.js
@@ -4,7 +4,8 @@ var app = require("../../config/server"),
     redis = require("redis"),
     client = redis.createClient(9111, "barb.redistogo.com"),
     GameSituation = require("../models/gameSituation.js"),
-    Player = require("../models/player.js");
+    Player = require("../models/player.js"),
+    Game = require("../models/game.js");
 
 // authenticate redis db
 client.auth("93fbe3baf4c48b4ec1b3a4f5522937c8");
@@ -19,9 +20,49 @@ app.get('/', function(req, res, next) {
 
 
 app.post('/fcb/games', function(req, res, next) {
+  // log incoming
   console.log("received games:");
   console.log(req.body);
   
+  // load the players (only nicknames)
+  var players = [];
+  client.hkeys("FCB", function (err, replies) {
+    _.each(replies, function(nickname) {
+      console.log("loading player: " + nickname);
+      players.push(nickname);
+    });
+    
+    var groupedList = _.groupBy(req.body.list, function(entry) {
+      return entry.date;
+    });
+  
+    var games = [];
+    var errors = [];
+    for ( var date in groupedList ) {
+      var game = new Game(groupedList[date]);
+      game.parse();
+      game.validate(players);
+      errors = _.union(errors, game.validationErrors);
+      //console.log(game);
+      games.push(game);
+    }
+    
+    if ( errors.length !== 0 ) {
+      var errorString = "";
+      for ( i = 0; i < errors.length; i++ ) {
+        errorString = errorString + "validation error:\n" + errors[i] + "\n";
+      }
+      return res.send(errorString, 500);
+    } else {
+      // redis operations
+      client.del("Games"); // delete the players hash
+      for ( i = 0; i < games.length; i++ ) {
+        client.hset("Games", games[i].date.toString(), JSON.stringify(games[i]));
+      }
+      return res.send("OK", 200);
+    }
+    
+  });
 });
 
 // updates the FCB Kader
@@ -35,7 +76,7 @@ app.post('/fcb/players', function(req, res, next) {
     var player = new Player("FCB", req.body.list[i]);
     player.parse();
     player.validate();
-    if ( player.validationErrors.length == 0 ) {
+    if ( player.validationErrors.length === 0 ) {
       players.push(player);
     } else {
       errors = _.union(errors, player.validationErrors);
@@ -44,7 +85,7 @@ app.post('/fcb/players', function(req, res, next) {
   
   if ( errors.length > 0 ) {
     var errorString = "";
-    for ( var i = 0; i < errors.length; i++ ) {
+    for ( i = 0; i < errors.length; i++ ) {
       errorString = errorString + "validation error:\n" + errors[i] + "\n";
     }
     
@@ -52,7 +93,7 @@ app.post('/fcb/players', function(req, res, next) {
   } else {
     // redis operations
     client.del("FCB"); // delete the players hash
-    for ( var i = 0; i < players.length; i++ ) {
+    for ( i = 0; i < players.length; i++ ) {
       client.hset("FCB", players[i].nickname, JSON.stringify(players[i]));
     }
   
@@ -79,10 +120,10 @@ app.post('/fcb/situations', function(req, res, next) {
       var gameSituation = new GameSituation(req.body.list[i]);
       gameSituation.parse();
       gameSituation.validate(players);
-      if ( gameSituation.validationErrors.length != 0 ) {
+      if ( gameSituation.validationErrors.length !== 0 ) {
         errors = _.union(errors, gameSituation.validationErrors);
       }
-      if ( gameSituation.parseErrors.length != 0) {
+      if ( gameSituation.parseErrors.length !== 0) {
         errors = _.union(errors, gameSituation.parseErrors);
       }
       gameSituations.push(gameSituation);
@@ -90,14 +131,14 @@ app.post('/fcb/situations', function(req, res, next) {
 
     if ( errors.length > 0 ) {
       var errorString = "";
-      for ( var i = 0; i < errors.length; i++ ) {
+      for ( i = 0; i < errors.length; i++ ) {
         errorString = errorString + "validation error:\n" + errors[i] + "\n";
       }
       return res.send(errorString, 500);
     } else {
       // redis operations
       client.del("Situations"); // delete the situations hash
-      for ( var i = 0; i < gameSituations.length; i++ ) {
+      for ( i = 0; i < gameSituations.length; i++ ) {
         client.hset("Situations", gameSituations[i].line, JSON.stringify(gameSituations[i]));
       }
       
---
+++ app/models/game.js
@@ -0,0 +1,112 @@
+//-----------------------------------------------------------------------------
+/*!
+    \file     game.js
+    \author   Gabriel Hase (gabriel.hase(at)upfront(dot)io)
+
+            purpose:    Defines a game.
+                        - parse
+                        - validate
+                        
+*/                  
+//-----------------------------------------------------------------------------
+
+var _ = require('underscore')._;
+
+
+//-----------------------------------------------------------------------------
+// C L A S S   G A M E
+//-----------------------------------------------------------------------------
+var Game = function(spreadsheetNotation) {
+  this.spreadsheetNotation = spreadsheetNotation;
+  this.players = [];
+  this.validationErrors = [];
+  
+  
+  // instance methods
+  //-----------------------------------------------------------------------------
+  // parses the spreadsheet notation into object notation (notation is an array)
+  //-----------------------------------------------------------------------------
+  this.parse = function() {
+    for ( var i = 0; i < this.spreadsheetNotation.length; i++ ) {
+      // just overwrite on each loop, should be everywhere the same
+      this.date = new Date( Date.parse(this.spreadsheetNotation[i].date) );
+      this.opponent = this.spreadsheetNotation[i].opponent;
+      this.competition = this.spreadsheetNotation[i].competition;
+      if ( this.spreadsheetNotation[i].homematch === 'h' ) {
+        this.homematch = true;
+      } else {
+        this.homematch = false;
+      }
+      this.finalScore = this.spreadsheetNotation[i].finalScore;
+      
+      // player stuff
+      var player = {};
+      player.name = this.spreadsheetNotation[i].player;
+      player.minutesPlayed = this.spreadsheetNotation[i].minutesPlayed;
+      player.grade = this.spreadsheetNotation[i].grade;
+      player.goals = this.spreadsheetNotation[i].goals;
+      player.assits = this.spreadsheetNotation[i].assists;
+      if ( this.spreadsheetNotation[i].yellowCard === 'x' ) {
+        player.yellowCard = true;
+      } else {
+        player.yellowCard = false;
+      }
+      if ( this.spreadsheetNotation[i].yellowRedCard === 'x' ) {
+        player.yellowRedCard = true;
+      } else {
+        player.yellowRedCard = false;
+      }
+      if ( this.spreadsheetNotation[i].redCard === 'x' ) {
+        player.redCard = true;
+      } else {
+        player.redCard = false;
+      }
+      if ( this.spreadsheetNotation[i].replacementFor !== '' ) {
+        player.replacementFor = this.spreadsheetNotation[i].replacementFor;
+      }
+      
+      this.players.push(player);
+      
+      // extra stuff
+      this.line = this.spreadsheetNotation[i].line;
+    }
+  }
+  
+  var validateCompleteTeam = function(players) {
+    console.log(this.spreadsheetNotation.length);
+    console.log(players.length);
+    if ( this.spreadsheetNotation.length !== players.length ) {
+      this.validationErrors.push("For the game of the " + this.date + " the team is not complete. There are " + this.spreadsheetNotation.length + " players in the notation, but there should be " + players.length);
+    }
+  };
+  
+  // validates if the total time played by all players is equal to 11 times the time of the game
+  var validateTimePlayed = function() {
+    var maxTime = 0;
+    var playedTime = 0;
+    for ( var i = 0; i < this.players.length; i++ ) {
+      if ( maxTime < this.players[i].minutesPlayed )
+        maxTime = this.players[i].minutesPlayed;
+      if ( this.players[i].minutesPlayed !== '' ) 
+        playedTime += this.players[i].minutesPlayed;
+    }
+    
+    var timeForAllPlayers = maxTime * 11;
+    if ( timeForAllPlayers !== playedTime )
+      this.validationErrors.push("The game played by all players does not add up to the total time. Check minutes played. The total amount of minutes played by all players (11 times time of the game) is " + timeForAllPlayers + " but the time recorded is " + playedTime);
+    
+  }
+  
+  //-----------------------------------------------------------------------------
+  // validates the fields
+  //-----------------------------------------------------------------------------
+  this.validate = function(players) {
+    // NOTE: -> the team can change. unless we only validate the last game this is not working
+    // validateCompleteTeam.call(this, players); 
+    validateTimePlayed.call(this);
+  }
+  
+  
+};
+
+module.exports = Game;
