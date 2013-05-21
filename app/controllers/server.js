c0d0b14a46497fd5aacaff706617e67be80e20c1
Gabriel <gabriel.hase@gmail.com>
2013-05-21 17:12:08 +0200

    Adds date to player statistics

--- app/controllers/server.js
+++ app/controllers/server.js
@@ -20,25 +20,25 @@ var logIncoming = function(req, res, next) {
 // H E L P E R S
 //-----------------------------------------------------------------------------
 var initStatistics = function(player) {
-  return { "played":            0, 
-           "goals":             0, 
-           "assists":           0, 
-           "minutes":           0, 
-           "yellowCards":       0, 
-           "yellowRedCards":    0, 
-           "redCards":          0, 
+  return { "played":            0,
+           "goals":             0,
+           "assists":           0,
+           "minutes":           0,
+           "yellowCards":       0,
+           "yellowRedCards":    0,
+           "redCards":          0,
            "order":             player.line,
            "name":              player.name,
            "nickname":          player.nickname,
            "number":            player.number,
            "imageUrl":          player.imageUrl,
            "position":          player.position,
-           "grades":            [], 
-           "scores":            [],  
+           "grades":            [],
+           "scores":            [],
            "minutesList":       [] };
 };
 
-var addGameToPlayersStatistic = function(player, playerStatistics, opponent) {
+var addGameToPlayersStatistic = function(player, playerStatistics, opponent, date) {
   playerStatistics[player.name].minutesList.push(+player.minutesPlayed);
   playerStatistics[player.name].minutes += +player.minutesPlayed;
   if ( +player.minutesPlayed > 0 ) {
@@ -46,9 +46,9 @@ var addGameToPlayersStatistic = function(player, playerStatistics, opponent) {
   }
   playerStatistics[player.name].goals += +player.goals;
   playerStatistics[player.name].assists += +player.assists;
-  playerStatistics[player.name].grades.push({ grade: +player.grade, gameAverageGrade: 0, opponent: opponent });
+  playerStatistics[player.name].grades.push({ grade: +player.grade, gameAverageGrade: 0, opponent: opponent, date: date });
   playerStatistics[player.name].scores.push({ scores: new Array( +player.assists, +player.goals ), opponent: opponent });
-  
+
 
   if ( player.yellowCard )
     playerStatistics[player.name].yellowCards += 1;
@@ -58,14 +58,14 @@ var addGameToPlayersStatistic = function(player, playerStatistics, opponent) {
     playerStatistics[player.name].redCards += 1;
 };
 
-var addGameAverageGradeToPlayersStatistics = function(gameGrades, gameEntry, playerStatistics) {  
+var addGameAverageGradeToPlayersStatistics = function(gameGrades, gameEntry, playerStatistics) {
   var sum = _.reduce(gameGrades, function(sum, grade) {
     return sum + grade;
   }, 0);
   var gameAverageGrade = sum / gameGrades.length;
   if ( _.isNaN(gameAverageGrade) )
     gameAverageGrade = 0;
-  
+
   // push to player statistics
   for ( var i = 0; i < gameEntry.players.length; i++ ) {
     var player = gameEntry.players[i];
@@ -78,7 +78,7 @@ var addGameAverageGradeToPlayersStatistics = function(gameGrades, gameEntry, pla
 
 var matchesGameFilter = function(game, filters) {
   var matchesFilter = true; // true by default
-  
+
   // look always for first filter
   if ( filters.location ) {
     if ( (filters.location === "home" && game.homematch === true) ||
@@ -88,24 +88,24 @@ var matchesGameFilter = function(game, filters) {
       matchesFilter = false;
     }
   }
-  
+
   // look only for second filter if first filter does not rule out yet
   if ( filters.competition && matchesFilter == true ) {
     if ( (filters.competition === "m" && game.competition === "m" ) ||
-         (filters.competition === "c" && game.competition === "c") || 
+         (filters.competition === "c" && game.competition === "c") ||
          (filters.competition === "euro" && (game.competition == "qcl" || game.competition == "el" || game.competition == "cl" || game.competition == "qel")) ){
       matchesFilter = true;
     } else {
       matchesFilter = false;
     }
   }
-  
+
   return matchesFilter;
 };
 
 var calcAverageGrade = function(grades) {
   var gradedCount = 0;
-  var sum = _.reduce(grades, function(memo, gradeEntry) { 
+  var sum = _.reduce(grades, function(memo, gradeEntry) {
     if ( gradeEntry && gradeEntry.grade && gradeEntry.grade > 0 ) {
       gradedCount += 1;
       return memo + gradeEntry.grade;
@@ -136,7 +136,7 @@ app.get("/testdata/games", function(req, res, next) {
       var game = JSON.parse(replies[key]);
       rawData.list.push(game.spreadsheetNotation);
     }
-    
+
     return res.json( rawData, 200 );
   });
 });
@@ -149,7 +149,7 @@ app.get("/testdata/situations", function(req, res, next) {
       var situation = JSON.parse(replies[key]);
       rawData.list.push(situation.spreadsheetNotation);
     }
-    
+
     return res.json( rawData, 200 );
   });
 });
@@ -162,13 +162,13 @@ app.get("/testdata/players", function(req, res, next) {
       var player = JSON.parse(replies[key]);
       rawData.list.push(player.spreadsheetNotation);
     }
-    
+
     return res.json( rawData, 200 );
   });
 });
 
 // get all saved situations
-app.get("/fcb/situations", function(req, res, next) {    
+app.get("/fcb/situations", function(req, res, next) {
     app.redisClient.hgetall("Situations", function(err, replies) {
       var gameSituationsExternal = {};
       gameSituationsExternal.list = [];
@@ -208,7 +208,7 @@ app.get("/fcb/statistics", function(req, res, next) {
       var player = JSON.parse(players[rawPlayer]);
       playerStatistics[player.name] = initStatistics(player);
     }
-    
+
     app.redisClient.hgetall("Games", function(err, games) {
       games = _.sortBy(games, function(game){
         return game;
@@ -222,55 +222,55 @@ app.get("/fcb/statistics", function(req, res, next) {
           var gameGrades = [];
           for ( var i = 0; i < gameEntry.players.length; i++ ) {
             var player = gameEntry.players[i];        // an entry from the player collection in a game -> one player in one game
-            if ( playerStatistics[player.name] ) {    // only do statistics for the current Kader              
+            if ( playerStatistics[player.name] ) {    // only do statistics for the current Kader
               // add the game to the statistics of this player
-              addGameToPlayersStatistic(player, playerStatistics, gameEntry.opponent);
-              
+              addGameToPlayersStatistic(player, playerStatistics, gameEntry.opponent, gameEntry.date);
+
               // add to grades
               if ( +player.grade > 0 ) {
                 gameGrades.push(+player.grade);
               }
-              
+
               // remove the player from the list of players that get an empty entry for this game
               //var idx = playersNotListed.find(player.name);
               //if ( idx != -1 ) visibleIds.splice(idx, 1);
-              
-            } // TODO if player is not listed it should add an empty entry  
+
+            } // TODO if player is not listed it should add an empty entry
           }
-          
+
           // TODO fill in empty records for player in the kader that didn't play
           /*
           for ( var i = 0; i < playersNotListed; i++ ) {
-            
+
           }
           */
-          
+
           // average calculation
           addGameAverageGradeToPlayersStatistics(gameGrades, gameEntry, playerStatistics);
         }
       }
-    
+
       // calc the average grade and package into array
       for ( var key in playerStatistics ) {
         // fill up with 0's for players that were not in the Kader yet
         while ( gamesCount > playerStatistics[key].grades.length ) {
           playerStatistics[key].grades.unshift( { grade: 0, averageGameGrade: 0, opponent: "Noch nicht im Kader" });
-          
+
         }
-        
+
         // same for scores
         while ( gamesCount > playerStatistics[key].scores.length ) {
           playerStatistics[key].scores.unshift( { scores: [ 0, 0 ], opponent: "Noch nicht im Kader" });
         }
-        
+
         playerStatistics[key].averageGrade = calcAverageGrade(playerStatistics[key].grades);
       }
-      
+
       // sort by the order
       var playerStatisticsList = _.sortBy(playerStatistics, function(player) {
         return player.order;
       });
-      
+
       return res.json( { list: playerStatisticsList }, 200 );
     });
   });
@@ -285,7 +285,7 @@ app.post('/fcb/games', logIncoming, function(req, res, next) {
 });
 
 // updates the FCB Kader
-app.post('/fcb/players', logIncoming, function(req, res, next) {  
+app.post('/fcb/players', logIncoming, function(req, res, next) {
   Player.parseValidateAndSaveSpreadsheet(app.redisClient, req.body.list, function(message, status) {
     console.log(message);
     return res.send(message, status);
